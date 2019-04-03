d795a281db93c325e94285fd740d3bed8436d271
Administrator <2478794031@qq.com>
2019-04-03 08:39:21 +0800

    my commit

---
+++ jianyue/pages/setting/setting.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class="container">
+		<view class="list">
+			<view class="list-item"><text>文章推送</text></view>
+			<view class="list-item"><text>新消息推送</text></view>
+			<view class="list-item"><text>个人资料</text></view>
+			<view class="list-item">
+				<button type="primary" @tap="logout">退出当前账号</button>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	onLoad() {
+		uni.setNavigationBarTitle({
+			title: '设置'
+		});
+	},
+	methods: {
+		logout: function() {
+			console.log('log out');
+			uni.removeStorageSync('login_key');
+			uni.showToast({
+				title: '已经退出当前账号'
+			});
+			uni.navigateBack();
+		}
+	}
+};
+</script>
+
+<style>
+</style>
---
+++ jianyue/pages/signin/signin.vue
@@ -0,0 +1,87 @@
+<template>
+	<view class="uni-flex uni-column container">
+		<input
+			class="uni-input"
+			type="number"
+			placeholder="输入手机号"
+			v-model="userDTO.mobile"
+			required="required"
+		/>
+		<input
+			class="uni-input"
+			password
+			type="text"
+			placeholder="输入密码"
+			v-model="userDTO.password"
+			required="required"
+		/>
+		<button type="primary" @tap="signIn(userDTO)">登录</button>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			userDTO: {
+				mobile: '',
+				password: ''
+			}
+		};
+	},
+	onLoad() {
+		uni.setNavigationBarTitle({
+			title: '登录'
+		});
+	},
+	methods: {
+		signIn: function(userDTO) {
+			var _this = this;
+			// console.log(userDTO.mobile + ',' + userDTO.password);
+			uni.request({
+				url: 'http://39.96.182.225:8080/api/user/sign_in',
+				method: 'POST',
+				data: {
+					mobile: userDTO.mobile,
+					password: userDTO.password
+				},
+				header: {
+					'content-type': 'application/json'
+				},
+				success: res => {
+					// console.log(res.data.data);
+					if (res.data.code == 0) {
+						//将用户数据记录在本地存储
+						uni.setStorageSync('login_key', {
+							userId: res.data.data.id,
+							nickname: res.data.data.nickname,
+							avatar: res.data.data.avatar,
+							token: res.data.data.token,
+							login: true
+						});
+						uni.showToast({
+							title: '登录成功'
+						});
+						uni.navigateBack();
+					}
+					//登录失败，弹出各种原因
+					else {
+						uni.showModal({
+							title: '提示',
+							content: res.data.msg
+						});
+					}
+				}
+			});
+		}
+	}
+};
+</script>
+
+<style scoped>
+input {
+	height: 50px;
+	border-bottom: 1px solid #eee;
+	margin-bottom: 5px;
+}
+</style>
